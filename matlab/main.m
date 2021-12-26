dff655fab81d220b5c0516b4cffcd4faa1fa8ce9
Malo Ranzetti <maloranzetti@gmail.com>
2021-12-26 22:06:32 +0100

    Task 3

--- matlab/main.m
+++ matlab/main.m
@@ -7,23 +7,27 @@ clear
 % ======================================================================= %
 % Simulation Options
 % ======================================================================= %
-N = 1e5;  % 5 simulate N bits each transmission (one block)
+N = 3e5;  % 5 simulate N bits each transmission (one block)
 maxNumErrs = 100; % get at least 100 bit errors (more is better)
-maxNum = 1e6; % 6 OR stop if maxNum bits have been simulated
+maxNum = 3e6; % 6 OR stop if maxNum bits have been simulated
 EbN0 = -1:8; % power efficiency range:
 
 % ======================================================================= %
 % Other Options
 % ======================================================================= %
-constellation = SymbolMapper.QPSK_GRAY; % Choice of constellation
-e1 = ConvEncoder.E1;
-e2 = ConvEncoder.E2;
-e3 = ConvEncoder.E3;
-UPPER_BOUND_DEPTH = 10; % How far we are willing to go in precision for the upper bound
 decoder_type = DecoderType.SOFT;
-decoder1 = ViterbiDecoder(e1.trellis, decoder_type, constellation);
-decoder2 = ViterbiDecoder(e2.trellis, decoder_type, constellation);
-decoder3 = ViterbiDecoder(e3.trellis, decoder_type, constellation);
+
+cons1 = SymbolMapper.BPSK;
+enc1 = ConvEncoder.E3;
+dec1 = ViterbiDecoder(enc1.trellis, decoder_type, cons1);
+
+cons2 = SymbolMapper.QPSK_GRAY;
+enc2 = ConvEncoder.E3;
+dec2 = ViterbiDecoder(enc2.trellis, decoder_type, cons2);
+
+cons3 = SymbolMapper.AMPM;
+enc3 = ConvEncoder.E4;
+dec3 = ViterbiDecoder(enc3.trellis, decoder_type, cons3);
 
 % ======================================================================= %
 % Simulation Chain
@@ -32,10 +36,19 @@ BER_coded1 = zeros(1, length(EbN0)); % pre-allocate a vector for BER results
 BER_coded2 = zeros(1, length(EbN0));
 BER_coded3 = zeros(1, length(EbN0));
 
+BER_uncoded1 = zeros(1, length(EbN0)); % pre-allocate a vector for BER results
+BER_uncoded2 = zeros(1, length(EbN0));
+BER_uncoded3 = zeros(1, length(EbN0));
+
 for i = 1:length(EbN0) % use parfor ('help parfor') to parallelize
   totErr1 = 0;  % Number of coded errors observed
   totErr2 = 0;  % Number of coded errors observed
   totErr3 = 0;  % Number of coded errors observed
+
+  totErr1uc = 0;  % Number of coded errors observed
+  totErr2uc = 0;  % Number of coded errors observed
+  totErr3uc = 0;  % Number of coded errors observed
+
   num = 0; % Number of bits processed
   snr = EbN0(i);
   
@@ -48,19 +61,26 @@ for i = 1:length(EbN0) % use parfor ('help parfor') to parallelize
   u = randi([0,1], N, 1);
 
   % [ENC] convolutional encoder
-  c1 = e1.encode(u);
-  c2 = e2.encode(u);
-  c3 = e3.encode(u);
+  c1 = enc1.encode(u);
+  c2 = enc2.encode(u);
+  c3 = enc3.encode(u);
 
   % [MOD] symbol mapper  
-  x1 = constellation.map(c1);
-  x2 = constellation.map(c2);
-  x3 = constellation.map(c3);
+  x1 = cons1.map(c1);
+  x2 = cons2.map(c2);
+  x3 = cons3.map(c3);
+  x1uc = cons1.map(u);
+  x2uc = cons2.map(u);
+  x3uc = cons3.map(u);
 
   % [CHA] add Gaussian noise
-  y1 = constellation.AWGN_channel(x1, snr);
-  y2 = constellation.AWGN_channel(x2, snr);
-  y3 = constellation.AWGN_channel(x3, snr);
+  y1 = cons1.AWGN_channel(x1, snr);
+  y2 = cons2.AWGN_channel(x2, snr);
+  y3 = cons3.AWGN_channel(x3, snr);
+
+  y1uc = cons1.AWGN_channel(x1uc, snr);
+  y2uc = cons2.AWGN_channel(x2uc, snr);
+  y3uc = cons3.AWGN_channel(x3uc, snr);
 
   % Only draw on the first iteration
 %   if drawFirst
@@ -70,18 +90,31 @@ for i = 1:length(EbN0) % use parfor ('help parfor') to parallelize
 %       plt = plot_symbols(y, constellation, snr);
 %   end
 
-  cf1 = decoder1.decode(y1);
-  cf2 = decoder2.decode(y2);
-  cf3 = decoder3.decode(y3);
+  cf1 = dec1.decode(y1);
+  cf2 = dec2.decode(y2);
+  cf3 = dec3.decode(y3);
+
+  cf1uc = cons1.unmap(y1uc);
+  cf2uc = cons2.unmap(y2uc);
+  cf3uc = cons3.unmap(y3uc);
   % ===================================================================== %
   % End processing one block of information
   % ===================================================================== %
   BitErrs1= sum(u~=cf1); % count the bit errors and evaluate the bit error rate
   BitErrs2 = sum(u~=cf2);
   BitErrs3 = sum(u~=cf3);
+  BitErrs1uc = sum(u~=cf1uc); % count the bit errors and evaluate the bit error rate
+  BitErrs2uc = sum(u~=cf2uc);
+  BitErrs3uc = sum(u~=cf3uc);
+
+
   totErr1 = totErr1 + BitErrs1;
   totErr2 = totErr2 + BitErrs2;
   totErr3 = totErr3 + BitErrs3;
+
+  totErr1uc = totErr1uc + BitErrs1uc;
+  totErr2uc = totErr2uc + BitErrs2uc;
+  totErr3uc = totErr3uc + BitErrs3uc;
   num = num + N; 
 
   disp(['+++ ' num2str((totErr1 + totErr2 + totErr3)/3) '/' num2str(maxNumErrs) ' errors. '...
@@ -91,6 +124,10 @@ for i = 1:length(EbN0) % use parfor ('help parfor') to parallelize
   BER_coded1(i) = totErr1/num;
   BER_coded2(i) = totErr2/num; 
   BER_coded3(i) = totErr3/num;
+
+  BER_uncoded1(i) = totErr1uc/num;
+  BER_uncoded2(i) = totErr2uc/num; 
+  BER_uncoded3(i) = totErr3uc/num;
 end
 % ======================================================================= %
 % End
@@ -102,17 +139,19 @@ end
 
 figure()
 hold on;
-plot(EbN0,e1.theoretical_BER_SOFT(UPPER_BOUND_DEPTH, EbN0),'Marker','x', 'Color',	'#9F0000')
-plot(EbN0,e2.theoretical_BER_SOFT(UPPER_BOUND_DEPTH, EbN0),'Marker','x', 'Color',	'#00741F')
 plot(EbN0, BER_coded1, 'Color', 'Red')
 plot(EbN0, BER_coded2, 'Color', 'Green')
-plot(EbN0, BER_coded3, 'Color', 'Blue')
+plot(EbN0, BER_coded3, 'Color', 'Cyan')
+
+plot(EbN0, BER_uncoded1, 'Color', '#AB0101', 'Marker','x')
+plot(EbN0, BER_uncoded2, 'Color', '#01AB31', 'Marker','x')
+plot(EbN0, BER_uncoded3, 'Color', '#1C01AB', 'Marker','x')
 
 
-title('Plot of coded BER for E1, E2, E3 compared to their BER upper bound')
+title('Plot of different systems with/without coding')
 xlabel('E_b/N_0 [dB]')
 ylabel('BER')
-legend('E1 - Upper bound', 'E2 & E3 - Upper bound', 'E1 - SOFT', 'E2 - SOFT', 'E3 - SOFT')
+legend('System 1 - CODED', 'System 2 - CODED', 'System 3 - CODED', 'System 1 - Uncoded', 'System 2 - Uncoded', 'System 3 - Uncoded')
 axis([EbN0(1) EbN0(end) 1e-4 1])
 set(gca, 'YScale', 'log')
 
